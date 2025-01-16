389954910468aaf786a5b174b317ccaad776c114
KrolikRoger1337 <dmitriem1@mail.ru>
2025-01-16 19:05:48 +0300

    Add files via upload
    Задание на Grade1: объявление переменных.

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = 'username'
+title = 'title'
+content = 'content'
+status = 'status'
+created_date = 'created_date'
+issue_date = 'issue_date'
+print(f'Имя пользователя: {username}')
+print(f'Заголовок заметки: {title}')
+print(f'Описание заметки: {content}')
+print(f'Статус заметки: {status}')
+print(f'Дата создания заметки в формате "день-месяц-год", например "10-11-2024":'
+      f' {created_date}')
+print(f'Дата истечения заметки (дедлайн) в формате "день-месяц-год", например "10-12-2024":'
+      f' {issue_date}')
